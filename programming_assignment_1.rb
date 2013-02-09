e5a23fa3a5101297bd9ada157af45408b2053b96
John Y <johnyu2@gmail.com>
2013-02-08 23:05:02 -0500

    programming assignment 1

---
+++ programming_assignment_1.rb
@@ -0,0 +1,7 @@
+# count number of inversions from IntegerArray.txt which contains 100,000 integers
+
+# input integers into two 50,000 arrays
+
+# sort arrays and count inversions
+
+# merge and add inversions
